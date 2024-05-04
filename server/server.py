4c1c3873f1305195809767fd2031ff064b9eb216
Syed Humayun Rasheed <104017282+humayunrasheed@users.noreply.github.com>
2024-05-04 18:40:34 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ import io
 import logging
 from flask import Flask
 from flask_cors import CORS
+from sklearn.model_selection import train_test_split  # Import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
